001280aa5109fdd4e10d27d5fa2bd769e69ef2a4
ammarhashmi113 <ammarhashmi113@gmail.com>
2025-07-19 23:40:50 +0500

    feat(backend): add catchAsync utility function which will be used to wrap async route handlers instead of repeated try/catch blocks

---
+++ backend/utils/catchAsync.js
@@ -0,0 +1,9 @@
+// utils/catchAsync.js
+
+const catchAsync = (fn) => {
+    return (req, res, next) => {
+        fn(req, res, next).catch((e) => next(e));
+    };
+};
+
+module.exports = catchAsync;
