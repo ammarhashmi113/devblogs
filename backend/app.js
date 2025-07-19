7cab6540b6030e30fbb7c815bb4c464360447bc7
ammarhashmi113 <ammarhashmi113@gmail.com>
2025-07-19 20:25:42 +0500

    feat(backend): add get route to get single blogpost from id

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,6 @@ const app = express();
 const Blog = require("./models/Blog");
 require("dotenv").config();
 
-console.log("PROCESS ENV-------------------------", process.env);
-
 // Connect with DB and serve API
 mongoose
     .connect(process.env.MONGO_URI)
@@ -27,13 +25,28 @@ app.get("/", (req, res) => {
     res.send("Hello Devs");
 });
 
-// GET all Blog posts
+// GET all blogposts
 app.get("/api/posts", async (req, res) => {
     try {
         const allBlogs = await Blog.find({});
-        res.json(allBlogs);
+        res.status(200).json(allBlogs);
     } catch (err) {
         console.error(err);
-        res.json(err);
+        res.status(500).json({ error: "Something went wrong." });
+    }
+});
+
+// GET a single blogpost from id
+app.get("/api/posts/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const blogFound = await Blog.findById(id);
+        if (!blogFound) {
+            return res.status(404).json({ error: "Blog not found." });
+        }
+        res.status(200).json(blogFound);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Something went wrong." });
     }
 });
