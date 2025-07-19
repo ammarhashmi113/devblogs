ccbea68f7aba03accc413ccefed491e13deef94c
ammarhashmi113 <ammarhashmi113@gmail.com>
2025-07-19 19:07:03 +0500

    feat(backend): mongoose connection setup in app.js, serve API after DB is connected, install dotenv, and moved MONGO_URI and PORT in .env file

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
-const port = 3000;
+
+// Connect with DB and serve API
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB Connected Successfully");
+
+        // Start server after DB is connected
+        app.listen(process.env.PORT, () => {
+            console.log(`Devlog API is Listening on Port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB Connection Error", err);
+    });
 
 app.get("/", (req, res) => {
     res.send("Hello Devs");
-});
-
-app.listen(port, () => {
-    console.log(`Devlog API is Listening on Port 3000`);
 });
