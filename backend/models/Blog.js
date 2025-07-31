abebfd42e6b9178be223eacfffd0f30f68b4bb6f
ammarhashmi113 <ammarhashmi113@gmail.com>
2025-07-31 20:27:47 +0500

    backend(feat): add imageUrl and tags to Blog model

--- backend/models/Blog.js
+++ backend/models/Blog.js
@@ -8,6 +8,17 @@ const blogSchema = new Schema(
         author: { type: Schema.Types.ObjectId, ref: "User", required: true },
         title: { type: String, required: true, trim: true, minlength: 5 },
         body: { type: String, required: true, trim: true, minlength: 10 },
+        imageUrl: { type: String, trim: true }, // image is optional
+        tags: {
+            type: [String],
+            validate: {
+                validator: function (arr) {
+                    return Array.isArray(arr) && arr.length > 0;
+                },
+                message: "A blog must have at least one tag.",
+            },
+            set: (tags) => tags.map((tag) => tag.trim().toLowerCase()),
+        },
         comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
         likes: [{ type: Schema.Types.ObjectId, ref: "Like" }],
     },
