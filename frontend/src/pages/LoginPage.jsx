ee693dc3466efa2bc75ecd85b44f8442eab0d172
ammarhashmi113 <ammarhashmi113@gmail.com>
2025-08-02 17:23:07 +0500

    feat(ui): add improved ui to the LoginPage

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -13,11 +13,13 @@ function LoginPage() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
+        console.log(e);
         try {
             const res = await axios.post(`${apiUrl}/login`, {
                 email,
                 password,
             });
+            console.log(res.data.token);
             // localStorage.setItem("token", res.data.token);
             navigate("/");
         } catch (err) {
@@ -29,30 +31,103 @@ function LoginPage() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                name="email"
-                value={email}
-                placeholder="email"
-                onChange={(e) => {
-                    setEmail(e.target.value);
-                }}
-            />
-            <input
-                type="password"
-                name="password"
-                value={password}
-                placeholder="password"
-                onChange={(e) => {
-                    setPassword(e.target.value);
-                }}
-            />
-            <button type="submit" disabled={loading}>
-                Submit
-            </button>
-            {error && <p className="text-red-500">{error}</p>}
-        </form>
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    alt="Devblogs"
+                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                    className="mx-auto h-10 w-auto"
+                />
+                <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                    Sign in to your account
+                </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form
+                    onSubmit={handleSubmit}
+                    method="POST"
+                    className="space-y-6"
+                >
+                    <div>
+                        <label
+                            htmlFor="email"
+                            className="block text-sm/6 font-medium text-gray-900"
+                        >
+                            Email address
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                value={email}
+                                placeholder="someone@example.com"
+                                required
+                                autoComplete="email"
+                                onChange={(e) => {
+                                    setEmail(e.target.value);
+                                }}
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label
+                                htmlFor="password"
+                                className="block text-sm/6 font-medium text-gray-900"
+                            >
+                                Password
+                            </label>
+                            <div className="text-sm">
+                                <a
+                                    href="#"
+                                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                                >
+                                    Forgot password?
+                                </a>
+                            </div>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                value={password}
+                                placeholder="••••••••"
+                                required
+                                autoComplete="current-password"
+                                onChange={(e) => {
+                                    setPassword(e.target.value);
+                                }}
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Sign in
+                        </button>
+                    </div>
+                </form>
+
+                <p className="mt-10 text-center text-sm/6 text-gray-500">
+                    Not a member?{" "}
+                    <a
+                        href="#"
+                        className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
+                        Become One
+                    </a>
+                </p>
+            </div>
+        </div>
     );
 }
 
