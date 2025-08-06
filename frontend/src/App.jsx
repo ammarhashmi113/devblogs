fb7eed9becbf8bec8f76d7f50b8d4bd900be5701
ammarhashmi113 <ammarhashmi113@gmail.com>
2025-08-06 20:13:39 +0500

    feat(frontend): add user auth context and auto-fetch current user on app load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,53 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import BlogsPage from "./pages/BlogsPage";
-import BlogDetailsPage1 from "./pages/BlogDetailsPage";
-import BlogDetailsPage from "./pages/BlogDetailsPage/BlogDetailsPage";
-import LoginPage from "./pages/LoginPage";
 import { Toaster } from "react-hot-toast";
 
+import api from "./utils/axiosConfig";
+import { UserContext } from "./contexts/userContext";
+
+import BlogsPage from "./pages/BlogsPage";
+import BlogDetailsPage from "./pages/BlogDetailsPage/BlogDetailsPage";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
+    const [user, setUser] = useState(null);
+    const [userLoading, setUserLoading] = useState(true);
+
+    async function fetchCurrentUser() {
+        try {
+            const res = await api.get("/me"); // for getting user info
+            setUser(res.data.user);
+        } catch (err) {
+            localStorage.removeItem("token"); // maybe it failed bcz token expired, so removing the token
+            setUser(null); // logging the user out
+        } finally {
+            setUserLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            fetchCurrentUser(); // fetching current user if token exists
+        } else {
+            setUserLoading(false);
+        }
+    }, []); // this useEffect runs only when the app starts
+
     return (
         <>
             <Toaster />
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<BlogsPage />} />
-                    <Route path="/blogs/:id" element={<BlogDetailsPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                </Routes>
+                <UserContext.Provider value={{ user, setUser, userLoading }}>
+                    <Routes>
+                        <Route path="/" element={<BlogsPage />} />
+                        <Route
+                            path="/blogs/:id"
+                            element={<BlogDetailsPage />}
+                        />
+                        <Route path="/login" element={<LoginPage />} />
+                    </Routes>
+                </UserContext.Provider>
             </BrowserRouter>
         </>
     );
