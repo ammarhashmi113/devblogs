91616af3e50f14ec89cf61904476dffd06afcfe4
ammarhashmi113 <ammarhashmi113@gmail.com>
2025-08-23 00:30:55 +0500

    feat(ui): update ui for BlogMainContentSkeleton

--- frontend/src/skeletons/BlogMainContentSkeleton.jsx
+++ frontend/src/skeletons/BlogMainContentSkeleton.jsx
@@ -2,28 +2,30 @@ import { Skeleton, SVGSkeleton } from "./Skeleton";
 
 const BlogMainContentSkeleton = () => (
     <>
-        <div className="lg:col-span-2 space-y-8">
+        <div className="lg:col-span-2 space-y-9 sm:space-y-8">
             <SVGSkeleton className="aspect-[16/9] w-full rounded-lg mb-12" />
-            <h1 className="leading-snug">
+            <h1 className="leading-snug space-y-4">
                 <Skeleton className="w-[432px] max-w-full" />
+                <Skeleton className="w-[300px] max-w-full sm:hidden" />
+                <Skeleton className="w-[232px] max-w-full sm:hidden" />
             </h1>
             <div className="flex items-center space-x-4 sm:space-x-6">
                 <div className="flex items-center gap-1 sm:gap-2">
-                    <SVGSkeleton className="sm:w-4 sm:h-4 w-[24px] h-[24px]" />
-                    <span>
-                        <Skeleton className="w-[88px] max-w-full" />
-                    </span>
-                </div>
-                <div className="flex items-center gap-1 sm:gap-2">
-                    <SVGSkeleton className="sm:w-4 sm:h-4 w-[24px] h-[24px]" />
+                    <SVGSkeleton className="w-3 h-3 sm:w-4 sm:h-4" />
                     <span>
                         <Skeleton className="w-[64px] max-w-full" />
                     </span>
                 </div>
                 <div className="flex items-center gap-1 sm:gap-2">
-                    <SVGSkeleton className="sm:w-4 sm:h-4 w-[24px] h-[24px]" />
+                    <SVGSkeleton className="w-3 h-3 sm:w-4 sm:h-4" />
                     <span>
-                        <Skeleton className="w-[80px] max-w-full" />
+                        <Skeleton className="w-[64px] max-w-full" />
+                    </span>
+                </div>
+                <div className="flex items-center gap-1 sm:gap-2">
+                    <SVGSkeleton className="w-3 h-3 sm:w-4 sm:h-4" />
+                    <span>
+                        <Skeleton className="w-[64px] max-w-full" />
                     </span>
                 </div>
             </div>
